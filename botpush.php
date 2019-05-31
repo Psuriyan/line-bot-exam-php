05a783589c58a76d374ccd3569bf4e1ae0b00afa
Psuriyan <Psuriyan@users.noreply.github.com>
2019-06-01 06:06:36 +0700

    Add files via upload

--- botpush.php
+++ botpush.php
@@ -4,11 +4,11 @@
 
 require "vendor/autoload.php";
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'dsORX2KZYrKs432P6skFyVUnYvx985XmGlYYrSIGlCqlntpOEJhBbEOptUytAWup/2v3k4+NlOcU2yTVJmlu2Vk+GQ2OTQ4x+yKpQOyAZjuGKnOjG4jXzK2YYZ0MKMhtL3xg0GZuPVFb+AKCOpQ6vgdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = '99749ba9d40c4b049a423cae3893876c';
 
-$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$pushID = 'U8a5c93f77732ebf1485ad3d6b4495136';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
